40ba57e0efe65cd3a3d27d30cf1f5bf250f66dd1
abasheryassin <abasheryassin@gmail.com>
2023-03-20 14:00:15 -0500

    CSS lodar and bable webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -39,7 +39,22 @@ module.exports = () => {
 
     module: {
       rules: [
-
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
